96d75883337ce3b3e5a7b9fa9e17ef70e0023368
Cristhian Duran <cristhian@logixboard.com>
2022-11-23 23:57:37 -0300

    Tired v100

--- pages/api/print.ts
+++ pages/api/print.ts
@@ -1,7 +1,8 @@
+import chromium from "chrome-aws-lambda";
 import multer from "multer";
 import type { NextApiRequest, NextApiResponse } from "next";
 import nextConnect from "next-connect";
-import { generatePdf } from "../../src/utils/print";
+import playwright from "playwright-core";
 
 type EndpointError = {
   message: string;
@@ -55,3 +56,89 @@ export const config = {
     responseLimit: false,
   },
 };
+
+const generatePdf = async (stickers: { buffer: Buffer }[]) =>
+  new Promise<Buffer | string>(async (resolve) => {
+    const options =
+      process.env.VERCEL_ENV === "production"
+        ? {
+            args: [
+              ...chromium.args,
+              "--hide-scrollbars",
+              "--disable-web-security",
+            ],
+            executablePath: await chromium.executablePath,
+            headless: true,
+          }
+        : {};
+
+    const browser = await playwright.chromium.launch(options);
+
+    let page = await browser.newPage();
+
+    await page.setViewportSize({ width: 2480, height: 3508 });
+
+    const css = `
+      html,
+      body {
+        line-height: 1;
+        margin: 0;
+      }
+
+      *,
+      *::before,
+      *::after {
+        box-sizing: border-box;
+      }
+
+      .grid {
+        display: flex;
+        flex-wrap: wrap;
+      }
+
+      .sticker-container {
+        height: calc(100vh / 4);
+        padding: 1px;
+        width: calc(100vw / 4);
+      }
+
+      .sticker {
+        display: block;
+        height: 100%;
+        margin: 0 auto;
+        width: 100%;
+      }
+    `;
+
+    const html = `
+          <style>
+            ${css}
+          </style>
+          <div class="grid">
+            ${stickers
+              .map(
+                (sticker) => `<div class="sticker-container">
+                  <img
+                    class="sticker"
+                    src="data:image/png;base64,${sticker.buffer.toString(
+                      "base64"
+                    )}"
+                  />
+                </div>`
+              )
+              .join("")}
+          </div>
+        `;
+
+    await page.setContent(html, {
+      waitUntil: "networkidle",
+    });
+
+    const buffer = await page.pdf({
+      format: "A4",
+    });
+
+    resolve(buffer);
+
+    await browser.close();
+  });
